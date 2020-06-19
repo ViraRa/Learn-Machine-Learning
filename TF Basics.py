30015e61eff17263562ff2b2121a87359b881808
Viraj Rana <virajrana@Virajs-MBP.hsd1.nj.comcast.net>
2020-06-19 16:13:59 -0400

    TF Basics.py

---
+++ TF Basics.py
@@ -0,0 +1,33 @@
+"""
+Learning Tensor Flow 2.0 Basics 
+
+Tensor Flow is a Python module created by Google that can do machine learning, 
+artificial intelligence and scientific computing. 
+
+Machine Learning is subset of AI. ML attempts to take the data and its output to 
+figure out a set of rules (a program). The goal is increase accouracy of models.
+Below is a general schematic
+
+Data --------->  ____________________
+                |                    |
+                |     ML Program     |    -----------> Rules  
+                |____________________|                   |
+OutPut ------->                                          |
+                                                         |
+                                                         |
+                                                         V  
+                                                        + Parameters
+                                                        w/ traditional programming
+                                                        |
+                                                        |
+                                                        |
+                                                        V
+                                                        Result
+""" 
+
+class ML: 
+
+    def __init__(self):
+        pass
+
+obj = ML()
